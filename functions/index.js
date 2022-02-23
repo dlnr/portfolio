e26072c0d1f8dca11fe537c8226fae9b364d87db
dlnr <niels@dlnr.dev>
2022-02-23 17:00:00 +0100

    hosting working, now for functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
-const functions = require("firebase-functions");
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://dlnr-dev.firebaseio.com',
+});
--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-analytics.js";
+import { getPerformance } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-performance.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-storage.js";
 import { getDatabase, connectDatabaseEmulator, ref, child, get, onValue } from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js';
 
 const firebaseConfig = {
@@ -9,40 +10,20 @@ const firebaseConfig = {
     projectId: 'dlnr-dev',
     storageBucket: 'dlnr-dev.appspot.com',
     messagingSenderId: '776926493994',
-    appId: '1:776926493994:web:be449c8aba4ccc32edf253',
-    // measurementId: 'G-S6D4D9WXCN',
+    appId: '1:776926493994:web:be449c8aba4ccc32edf253'
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const perf = getPerformance(app);
+const storage = getStorage(app);
 const db = getDatabase();
+
 if (location.hostname === "localhost") {
-  // Point to the RTDB emulator running on localhost.
   connectDatabaseEmulator(db, "localhost", 9000);
 }
 
-// const portfolio = db.ref('portfolio');
-
-// const portfolio = ref(db, 'portfolio/');
-
-
-// const dbRef = ref(db, 'portfolio');
-
-// onValue(dbRef, (snapshot) => {
-//   const data = snapshot.val();
-//   console.log(data);
-// });
-// get(child(dbRef, 'straightaway')).then((snapshot) => {
-//   if (snapshot.exists()) {
-//     console.log(snapshot.val());
-//   } else {
-//     console.log("No data available");
-//   }
-// }).catch((error) => {
-//   console.error(error);
-// });
-
 const dbRef = ref(db);
+
 get(child(dbRef, `portfolio`)).then((snapshot) => {
   console.log(snapshot);
   if (snapshot.exists()) {
@@ -54,8 +35,3 @@ get(child(dbRef, `portfolio`)).then((snapshot) => {
   console.error(error);
 });
 
-// ref.orderByChild('timestamp').on('child_added', (snapshot) => {
-//   console.log(snapshot.key + ' was ' + snapshot.val().height + ' meters tall');
-// });
-
-console.log('db', dbRef);
